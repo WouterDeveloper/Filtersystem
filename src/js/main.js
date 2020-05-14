69ac63555010e93819f8e4f4f856fb10b2e40106
WouterDeveloper <wouterwijnands@protonmail.com>
2020-05-14 15:20:13 +0200

    Added setup of project: installed npm and dependencies, wrote configs for dependencies and added basic structure and html/scss/js files

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+// Libraries
+import Vue from 'vue';
+
+// Scripts
+
+export default function app() {
+  Vue.config.devtools = true;
+// eslint-disable-next-line
+  new Vue({          
+      el: '#app',
+      data: {
+          return() {
+          },
+      },
+  });
+}
